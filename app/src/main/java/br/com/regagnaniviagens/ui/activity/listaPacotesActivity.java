77e549863db2d7a81d821524a1bef6e8df6a9849
Luiz_ <luiz.fernando.regagnani@gmail.com>
2022-10-29 17:07:16 -0300

    base de um app de agencia de viagens, layout em desenvolvimento

---
+++ app/src/main/java/br/com/regagnaniviagens/ui/activity/listaPacotesActivity.java
@@ -0,0 +1,15 @@
+package br.com.regagnaniviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.regagnaniviagens.R;
+
+public class listaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
